a877017b2460363493fc7031d6be4d4c104ff8d0
Emay-Pandarakutty <79270064+Emay-Pandarakutty@users.noreply.github.com>
2021-10-09 19:22:45 -0400

    Added Mock
    Mock function  added followed two methods 1. Decorator 2. Context Manger

--- Test.py
+++ Test.py
@@ -1,31 +1,43 @@
 """
-Date:10/02/2021
+Date:10/09/2021
 @author: Emay Pandarakutty
 email: epandar@stevens.edu
 
 Test for the main file.
 """
 import unittest
+from unittest import mock
 from Main import get_repos, get_commits, main
 
 user_id = 'Emay-Pandarakutty'
 invalid_user_id = 'Pandarakutty'
 
+
 class Test_Main(unittest.TestCase):
-    def test_get_repos_valid(self):
+    # decorator mock
+    @mock.patch('Main.get_repos', return_value=['HW04', 'ssw555b_team7', 'SSW567-HW00', 'Triangle567'])
+    def test_get_repos_valid(self, mock_get_repos):
         self.assertEqual(get_repos(user_id), ['HW04', 'ssw555b_team7', 'SSW567-HW00', 'Triangle567'])
 
-    def test_get_repos_invalid(self):
+    @mock.patch('Main.get_repos', return_value=r'repo url error')
+    def test_get_repos_invalid(self, mock_get_repos):
         self.assertEqual(get_repos(invalid_user_id), 'repo url error')
 
-    def test_get_commits_valid(self):
+    @mock.patch('Main.get_commits', return_value='Repo: ssw555b_team7  Number of commits: 30')
+    def test_get_commits_valid(self, mock_get_commit):
         self.assertEqual(get_commits(user_id, 'ssw555b_team7'), 'Repo: ssw555b_team7  Number of commits: 30')
-        self.assertEqual(get_commits(user_id, 'SSW567-HW00'), 'Repo: SSW567-HW00  Number of commits: 5')
+        # context Manger mock
+        with mock.patch('Main.get_commits', return_value=r'Repo: SSW567-HW00  Number of commits: 5'):
+            self.assertEqual(get_commits(user_id, 'SSW567-HW00'), 'Repo: SSW567-HW00  Number of commits: 5')
 
+    # context Manger mock
     def test_get_commits_invalid(self):
-        self.assertEqual(get_commits(user_id, 'SSW567-HW05'), 'commit url error')
+        with mock.patch('Main.get_commits', return_value=r'commit url error'):
+            self.assertEqual(get_commits(user_id, 'SSW567-HW05'), 'commit url error')
 
-    def test_main(self):
+    # decorator mock
+    @mock.patch('Main.main', return_value=None)
+    def test_main(self, mock_main):
         self.assertEqual(main(user_id), None)
 
 
